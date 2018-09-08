2128d522d98ae2ab95a200dcac16aa7429595007
John Colanduoni <john@colanduoni.com>
2018-09-08 02:01:28 -0700

    Simplify tests definition

--- tests/cases/mod.rs
+++ tests/cases/mod.rs
@@ -11,64 +11,47 @@ macro_rules! check_perm_failure {
 mod fs;
 mod rand;
 
-pub struct TestCases {
-    pub create_file_home: bool,
-    pub list_home_directory: bool,
-    pub open_extant_file_home: bool,
-    pub open_nonexistent_file_home: bool,
-    pub os_rng: bool,
-}
+macro_rules! define_cases {
+    ($($module:ident::$name:ident),*$(,)*) => {
+        pub struct TestCases {
+            $(pub $name: bool,)*
+        }
 
-macro_rules! run_case {
-    ($expected:expr => $f:expr) => {
-        match ::std::panic::catch_unwind(|| {
-            $f
-        }) {
-            ::std::result::Result::Ok(accessible) => if accessible == $expected {
-                eprintln!("test case {} passed", stringify!($expected));
-                true
-            } else {
-                eprintln!("test case {} was expected to {}, but it did not", stringify!($expected), if $expected { "succeed" } else { "fail" });
-                false
-            },
-            ::std::result::Result::Err(err) => {
-                eprintln!("test case {} panicked: {:?}", stringify!($expected), err);
-                false
+        impl TestCases {
+            pub fn none() -> TestCases {
+                TestCases {
+                    $($name: false,)*
+                }
+            }
+
+            pub fn run(&self) -> bool {
+                let mut all_passed = true;
+                $(
+                    match ::std::panic::catch_unwind(|| {
+                        $module::$name()
+                    }) {
+                        ::std::result::Result::Ok(accessible) => if accessible == self.$name {
+                            eprintln!("test case {} passed", stringify!($name));
+                        } else {
+                            eprintln!("test case {} was expected to {}, but it did not", stringify!($name), if self.$name { "succeed" } else { "fail" });
+                            all_passed = false;
+                        },
+                        ::std::result::Result::Err(err) => {
+                            eprintln!("test case {} panicked: {:?}", stringify!($name), err);
+                            all_passed = false;
+                        }
+                    }
+                )*
+                all_passed
             }
         }
-    };
+    }
 }
 
-macro_rules! run_cases {
-    ($($expected:expr => $f:expr;)*) => {
-        {
-            let mut passed = true;
-            $(
-                passed = run_case!($expected => $f) && passed;
-            )*
-            passed
-        }
-    };
-}
-
-impl TestCases {
-    pub fn none() -> TestCases {
-        TestCases {
-            create_file_home: false,
-            list_home_directory: false,
-            open_extant_file_home: false,
-            open_nonexistent_file_home: false,
-            os_rng: false,
-        }
-    }
-
-    pub fn run(&self) -> bool {
-        run_cases! {
-            self.create_file_home => fs::create_file_home();
-            self.list_home_directory => fs::list_home_directory();
-            self.open_extant_file_home => fs::open_extant_file_home();
-            self.open_nonexistent_file_home => fs::open_nonexistent_file_home();
-            self.os_rng => rand::os_rng();
-        }
-    }
+define_cases! {
+    fs::create_file_home,
+    fs::list_home_directory,
+    fs::open_extant_file_home,
+    fs::open_nonexistent_file_home,
+    rand::os_rng,
 }
